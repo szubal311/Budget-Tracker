9a35d6ec41ecfd19782e45a1695b3465ceb7db7c
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-04-24 14:51:01 -0600

    made up dates to the css added icons

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = 2020;
 
 const app = express();
 
@@ -26,5 +26,5 @@ mongoose.connect("mongodb://localhost/budget", {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port http://localhost:${PORT}`);
 });
