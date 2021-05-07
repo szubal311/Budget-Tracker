8b4105e3212ff3485c7193511ed88ea8ae7a2ff5
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-05-06 18:27:54 -0600

    updated mongo connect

--- server.js
+++ server.js
@@ -3,19 +3,22 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 2020;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(logger("dev"));
 
+// app.get("/", function(req, res) {
+//   res.json(path.join(__dirname, "./public/index.html"))
+// });
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
